f7907b08e4062b19522cda62eaac2b35cc76e6c7
lauanS <lauan.s.souza@gmail.com>
2021-05-31 04:20:53 -0300

    Visualização de um objeto 3D usando o OpenGL

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
 import numpy as np
 
-class ObjLoader(object):
+from OpenGL.GL import *
+import pygame
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import math
+
+class ObjLoader:
     def __init__(self):
         self.vertices = np.array([])
         self.faces = np.array([])
@@ -33,19 +39,88 @@ class ObjLoader(object):
             print(".obj file not found.")
     
     def show_3D_obj(self):
-        return 1;
+        return 1
+    
+    def render_scene(self):
+        if len(self.faces) > 0:
+            ##
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+            glBegin(GL_TRIANGLES)
+            for face in self.faces:
+                for f in face:
+                    vertexDraw = self.vertices[int(f) - 1]
+                    if int(f) % 3 == 1:
+                        glColor4f(0.282, 0.239, 0.545, 0.35)
+                    elif int(f) % 3 == 2:
+                        glColor4f(0.729, 0.333, 0.827, 0.35)
+                    else:
+                        glColor4f(0.545, 0.000, 0.545, 0.35)
+                    glVertex3fv(vertexDraw)
+            glEnd()
     
     def print(self):
         print("Vertices:")
         print(self.vertices)
         print("Faces:")
         print(self.faces)
-        
+
+class objItem:
+
+    def __init__(self, object):
+        self.angle = 0
+        self.vertices = []
+        self.faces = []
+        self.coordinates = [0, 0, -65]  # [x,y,z]
+        self.teddy = object
+        self.position = [0, 0, -50]
+
+
+    def render_scene(self):
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+        glClearColor(0.902, 0.902, 1, 0.0)
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+        gluLookAt(0, 0, 0, math.sin(math.radians(self.angle)), 0, math.cos(math.radians(self.angle)) * -1, 0, 1, 0)
+        glTranslatef(self.coordinates[0], self.coordinates[1], self.coordinates[2])
+
 def main():
     obj = ObjLoader()
     file_name = 'src/modelos3D/small.obj'
     obj.load_3D_obj(file_name)
     obj.print()
+    obj.render_scene()
 
+def main_guide():
+    pygame.init()
+    pygame.display.set_mode((640, 480), pygame.DOUBLEBUF | pygame.OPENGL)
+    pygame.display.set_caption("Ursinho super bonitinho")
+    clock = pygame.time.Clock()
+    # Feature checker
+    glDisable(GL_TEXTURE_2D)
+    glEnable(GL_DEPTH_TEST)
+    glEnable(GL_BLEND)
+    glEnable(GL_CULL_FACE)
+    #
+    glMatrixMode(GL_PROJECTION)
+    gluPerspective(45.0, float(800) / 600, .1, 1000.)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+    ursinho = ObjLoader()
+    ursinho.load_3D_obj('src/modelos3D/ursinho.obj')
+    objectTeddy = objItem(ursinho)
+
+    done = False
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+        objectTeddy.render_scene()
+        objectTeddy.teddy.render_scene()
+        pygame.display.flip()
+        clock.tick(30)
+    pygame.quit()
+    
 if __name__ == '__main__':
-    main()
+    main_guide()
