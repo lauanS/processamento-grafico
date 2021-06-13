4d944a871306cf07b5270814d86a1bc057cdb93b
lauanS <lauan.s.souza@gmail.com>
2021-06-13 19:35:23 -0300

    Desenhando com triangulos + correções de bugs

--- src/Render.py
+++ src/Render.py
@@ -7,21 +7,27 @@ from ObjView import ObjView
 from Scene import Scene
 from Camera import Camera
 
+
 class Render:
-    def __init__(self, object = {}):
+    def __init__(self, object={}):
         # Objeto que será renderizado
         self.object = object
         # Matriz da imagem (600 por 600 para a mão)
         # Use 100x100 para o ursinho
-        self.image = np.zeros((600, 600))
+        self.image = np.zeros((100, 100))
 
     def render(self):
         # Obtem as vertices do objeto (pontos)
         vertices = self.object.vertices
+        faces = self.object.faces
+
         # Para cada ponto, desenha ele na matrix, usando seu x e y
-        for i in range(len(vertices)):
-            startpoint = vertices[i]
-            self.draw_pixel(startpoint[0], startpoint[1])
+        for i in range(len(faces)):
+            point_a = vertices[int(faces[i][0]) - 1]
+            point_b = vertices[int(faces[i][1]) - 1]
+            point_c = vertices[int(faces[i][2]) - 1]
+            self.draw_triangle(point_a, point_b, point_c)
+            self.draw_triangle(-point_a, -point_b, -point_c)
         # Plota a matrix como uma imagem
         plt.imshow(self.image, interpolation='nearest')
         plt.show()
@@ -31,14 +37,14 @@ class Render:
         self.draw_line(point_a[0], point_a[1], point_b[0], point_b[1])
         self.draw_line(point_b[0], point_b[1], point_c[0], point_c[1])
         self.draw_line(point_c[0], point_c[1], point_a[0], point_a[1])
-    
+
     # Desenha uma linha na imagem
     def draw_line(self, x0, y0, x1, y1):
         dx = x1 - x0
         dy = y1 - y0
 
         # Valor inicial de d
-        d = 2 * dy - dx 
+        d = 2 * dy - dx
         # Incremento de E
         incE = 2 * dy
         # Incremento de NE
@@ -50,12 +56,12 @@ class Render:
 
         while(x < x1):
             # Escolhe E
-            if(d < x1):                
+            if(d <= 0):
                 d = d + incE
                 x = x + 1
             # Escolhe NE
-            else:                
-                d = d + incE
+            else:
+                d = d + incNe
                 x = x + 1
                 y = y + 1
             self.draw_pixel(x, y)
@@ -63,15 +69,16 @@ class Render:
     # Pinta um pixel na imagem
     def draw_pixel(self, x, y):
         # print(f'X: {x}[{int(x)}]|Y: {y}[{int(y)}]')
-        self.image[299 - int(x)][299 - int(y)] = 1
-        
-
+        self.image[int(len(self.image)/2) - int(x)
+                   ][int(len(self.image)/2) - int(y)] = 1
 
 
 def main():
     # -----------------------Carregando um objeto ----------------------- #
     obj = ObjLoader()
-    file_name = 'src/modelos3D/coarseTri.hand.obj'
+    pathHand = 'coarseTri.hand.obj'
+    urso = 'ursinho.obj'
+    file_name = 'src/modelos3D/' + urso
     obj.load_3D_obj(file_name)
 
     # -----------------------Criando uma cena----------------------- #
@@ -84,13 +91,14 @@ def main():
     # Criando uma câmera apontada para o objeto passado como parâmetro
     cam = Camera(obj)
     # Definindo posição da câmera, ponto a ser visualizado e o vetor de orientação respectivamente
-    cam.set_cam_info([5, 4, 3], [0.0, 10.0, 0.0], [0, 0, 1])
+    cam.set_cam_info([5, 4, 3], [1.0, 1.0, 1.0], [1, 1, 1])
     cam.set_perspective_info(2, 2, -4, -8.5)
     cam.transform_visualization()
     obj.vertices = cam.change_perspective()
     # Renderiza ele
     render = Render(obj)
     render.render()
+
 
 if __name__ == '__main__':
     main()
